3b524c9224c868d40528fd8a5bd94b4a7fbbd3f6
seba004 <sebastian.tapia@alumnos.usm.cl>
2014-06-01 18:19:03 -0400

    se agrega
    se agrega protocolo

--- tarea 2 redes/src/clienteTCP.java
+++ tarea 2 redes/src/clienteTCP.java
@@ -9,8 +9,9 @@ public class clienteTCP {
 	
 	 Socket conexion;
 	 final static int  port= 8086;
+	
 	 
-	 public clienteTCP(){
+	 public clienteTCP(String ip_out,String port_out,String ip_in, String port_in,String message){
 		 try {
 			this.conexion=new Socket("localhost",port);
 		} catch (UnknownHostException e) {
@@ -25,6 +26,29 @@ public class clienteTCP {
 	 public void sendmessage (String message) throws IOException{
 		 DataOutputStream outServer = new DataOutputStream(this.conexion.getOutputStream());
 		 outServer.writeBytes(message+'\n');
+		 outServer.flush();
+	 }
+	 
+	 public void say_hi (String ip) throws IOException{
+		 DataOutputStream outServer = new DataOutputStream(this.conexion.getOutputStream());
+		 outServer.writeByte('%'+'\n');//preguntar por mensajes simbolo = %
+		 outServer.writeBytes(ip+'\n');
+		 outServer.flush();
+	 }
+	 public void ask_message(String ip_out,String port_out, String ip_in, String port_in) throws IOException{
+		 String  chat= null;
+		 DataOutputStream outServer = new DataOutputStream(this.conexion.getOutputStream());
+		 outServer.writeByte('!'+'\n');//preguntar por mensajes simbolo = !
+		 outServer.writeBytes(ip_out+port_out+ip_in+port_in+'\n');//ip salida puerto salida ip llegada puerto llegda
+		 outServer.flush();
+		
+	 }
+	 
+	 public void send_message(String ip_out,String port_out, String ip_in, String port_in,String message) throws IOException{
+		
+		 DataOutputStream outServer = new DataOutputStream(this.conexion.getOutputStream());
+		 outServer.writeByte('@'+'\n');//chatear = @
+		 outServer.writeBytes(ip_out+port_out+ip_in+port_in+message+'\n');//ip salida puerto salida ip llegada puerto llegda
 		 outServer.flush();
 	 }
 	
