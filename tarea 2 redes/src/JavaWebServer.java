26b0b330a0a682b994d669a6b0c540fb62b409ca
seba004 <sebastian.tapia@alumnos.usm.cl>
2014-06-03 01:40:55 -0400

    unir y completar
    se unieron las funciones del cliente tcp  y deberia estar ok

--- tarea 2 redes/src/JavaWebServer.java
+++ tarea 2 redes/src/JavaWebServer.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter; 
 import java.net.ServerSocket; 
 import java.net.Socket;
+import java.util.StringTokenizer;
 import java.util.concurrent.Executor; 
 import java.util.concurrent.Executors; 
 
@@ -18,14 +19,14 @@ public class JavaWebServer
 {   
 	private static final int fNumberOfThreads = 100;	//para que el  servidor pueda tener varios clientes
 	private static final Executor fThreadPool = Executors.newFixedThreadPool(fNumberOfThreads);//para ejecutar hebras   
-	
-	
-	
+
+
+
 	static String direccion;
 	static String parametro;
  
     
-	
+
 	private static String corta(String palabra, String pedido){ //modificar para que me devuelva lo que yo quiero
 		int i=0;
 		String nombre = null;
@@ -33,8 +34,8 @@ public class JavaWebServer
 		String puerto = null;
 		String entrega = null;
 		String[] tuplas =palabra.split("&");
-		
-		 
+
+
 		 while ( i < tuplas.length){
 			 String[] valor= tuplas[i].split("=");
 			 if (valor[0].equals("nombre")){
@@ -48,39 +49,39 @@ public class JavaWebServer
 			 }
 			 i++;
 		 }
-		 
+
 		 if (pedido.equals("nombre")){
 			 entrega = nombre;			 
 		 }
-		
+
 		 if (pedido.equals("ip")){
 			 entrega = ip;			 
 		 }
 		 if (pedido.equals("puerto")){
 			 entrega = puerto;			 
 		 }
-		
+
 		 return entrega;
 	}
-	
+
 	private static void escritor(String palabro) throws IOException {
-		
+
 		String fichero = "contactos.html";
 	    FileWriter escri = new FileWriter("src/" + fichero,true); //valor true agregara los datos nuevos
 	    escri.write("<p>"+palabro+"</p>\r\n");// se guarda el nombre en el archivo de texto con formato html
 	    escri.close();
-		
+
 	}
-	
+
 	private static void escritor2(String palabro) throws IOException {
-		
+
 		String fichero = "todo.txt";
 	    FileWriter escri = new FileWriter("src/" + fichero,true); //valor true agregara los datos nuevos
 	    escri.write(palabro+"\n");// se guarda el nombre en el archivo de texto con formato html
 	    escri.close();
-		
+
 	}
-	
+
     public static void identificar(String direct) {
             	  
     	    
@@ -106,23 +107,23 @@ public class JavaWebServer
 	      BufferedReader br = null;
 	      String nombre;
 	      String entrega = null;
-	 
+
 	      try {
 	         archivo = new File ("src/todo.txt");
 	         fr = new FileReader (archivo);
 	         br = new BufferedReader(fr);
-	 
+
 	         // Lectura del fichero
 	         String linea;
 	         while((linea = br.readLine())!= null){
-	        	 
-	        		        	 
+
+
 	        	 nombre = corta(linea,"nombre");
-	        		        		        	 
+
 	        	 if(nombre.equals(contacto)){
 	        		 entrega = corta(linea,pedido);	        		 
-	        		 
-	        		 
+
+
 	        	 }
 	         }
 	        	// return entrega;
@@ -130,7 +131,7 @@ public class JavaWebServer
 	      catch(Exception e){
 	         e.printStackTrace();
 	      }finally{
-	        
+
 	         try{                    
 	            if( null != fr ){   
 	               fr.close();     
@@ -141,14 +142,14 @@ public class JavaWebServer
 	      }
 		return entrega;
 	   }
-	
-	
-	
-	
+
+
+
+
 	public static void main(String[] args) throws IOException 
 	{ 
 		ServerSocket socket = new ServerSocket(8083); // se crea socket en puerto designado
-		
+
 		while (true) 
 		{
 			final Socket connection = socket.accept(); //while  hasta que  exista conexion 
@@ -161,17 +162,17 @@ public class JavaWebServer
 				} 
 			};
 			fThreadPool.execute(task);
-			
+
 		}
-		
-		
-		
+
+
+
 	}   
-	
+
 	static String contacto = "no a seleccionado contacto"; // ver si resulta
 	static String ip_contacto;
 	static String port_contacto;
-	
+
 	private static void HandleRequest(Socket sockete)
 	{ 
 		BufferedReader in;
@@ -180,45 +181,45 @@ public class JavaWebServer
 		int postsize = 0;
 		try 
 		{ 
-			
+
 			in = new BufferedReader(new InputStreamReader(sockete.getInputStream())); 
 			out = new PrintWriter(sockete.getOutputStream(), true);
-			
+
 			while(true)
 			{
 
 				request = in.readLine(); 
-				
+
 				if(request != null) {
-									
-					
+
+
 					if(request.startsWith("GET") || request.startsWith("POST") ){
-						
+
 		   	        	String[] splitted = request.split("\\s+");
 		   		        identificar(splitted[1]);
-		   	             
+
 		   		         out.println("HTTP/1.0 200");
 		   		         //out.println("Content-type: text/html"); 
 		   		         out.println("Server-name: myserver"); 
-		   	             
+
 		   	             File file = new File(System.getProperty("user.dir") + direccion );
-		   	             
+
 		   	             FileInputStream fis = new FileInputStream(file);
-		   	             
+
 		   	             out.println("Content-length: " + file.length());
 		   	             out.println("");
 
 		   	             byte[] buffer = new byte[(int)file.length()];
-		   	             
+
 		   	             for(int i=0 ; i < file.length(); i++)
 		   	             {
 		   	            	 buffer[i] = (byte)fis.read();
 		   	             }
-		   	             
+
 		   	             sockete.getOutputStream().write(buffer);
 		   	             fis.close();
 		   	        }
-					
+
 					if(request.startsWith("Content-Length:")) {
 						postsize = Integer.parseInt(request.split("\\s+")[1]);
 					}
@@ -227,61 +228,94 @@ public class JavaWebServer
 	                    break;
 	                }
 				}
-				
+
 				out.flush();
 				out.close(); 
 				sockete.close();
-				
-				
-				
-				
+
+
+
+
 			}
-			
-			
-			
+
+
+
 			if(postsize > 0) {
 				char cont[] = new char[postsize];
 				in.read(cont);
 				String a = new String(cont);
-				
-				
-				
-				
+
+
+				clienteTCP clienton = new clienteTCP();
+				clienton.say_hi();
+				String respuesta = clienton.read_server();
+				StringTokenizer tokens = new StringTokenizer(respuesta,"¬¬");
+				String Protocolo =tokens.nextToken();
+				if (Protocolo.equals("hi_back")){
+					String massage =tokens.nextToken();
+					System.out.println(massage);
+				}
 				//System.out.println(contacto);
 				//System.out.println(a); //ver que se escribe en los "Post"
-				
+
 				if  (a.contains("%24")){ // $ quiere chatear con alguien	
-					
+
 				contacto = a.substring(10);
-							
+
 				ip_contacto = obtener(contacto,"ip");
 				port_contacto = obtener(contacto,"puerto");
 				
+				
+				clienton.ask_message( ip_contacto, port_contacto);//crear variables para las cosa sque se manda
+				String respuesta_peticion;
+				String fichero = "chat.html";
+			    FileWriter escri = new FileWriter("src/" + fichero,true); //valor true agregara los datos nuevos
+			   
+				while (clienton.read_server()!= null){
+					 respuesta_peticion = clienton.read_server();
+					StringTokenizer token = new StringTokenizer(respuesta_peticion,"¬¬");
+					String Protocolo1 =token.nextToken();
+					if(Protocolo1.equals("dispach_message")){
+						String chat =token.nextToken();
+						escri.write("<p>"+chat+"</p>\r\n");
+					}
+				}
+				 escri.close();
+
 				//System.out.println(ip_contacto);
 				//System.out.println(port_contacto);
-								
+
+				}
+
+				if(a.contains("%23")){ // esta escribiendo en el chat
+
+				String mensaje = a.substring(10);
+				clienton.send_message(ip_contacto, port_contacto, mensaje);
+				String ultima_respuesta = clienton.read_server();
+				StringTokenizer toke = new StringTokenizer(respuesta,"¬¬");
+				String Protocolo3 =toke.nextToken();
+				if (Protocolo3.equals("save_message")){
+					String massage =toke.nextToken();
+					System.out.println(massage);
 				}
 				
-				if(a.contains("%23")){ // esta escribiendo en el chat
-					
-				String mensaje = a.substring(10);
 				
 				//System.out.println(mensaje);
 				//System.out.println(contacto);
 				//System.out.println(ip_contacto);
 				//System.out.println(port_contacto);
-					
+
 				}
 				if(a.contains("&")){ // esta agregando un contacto
-				
+
 				escritor(corta(a,"nombre"));
 				escritor2(a);
 				postsize = 0;
-						
+
 				}
 			}
-			
-		
+
+
 		} 
 		catch (IOException e)
 		{ 
@@ -294,7 +328,7 @@ public class JavaWebServer
 				try 
 				{
 					sockete.close();
-					
+
 				} 
 				catch (IOException e)
 				{ 
@@ -304,5 +338,5 @@ public class JavaWebServer
 		} 
 		return; 
 	}   
-	
+
 }
